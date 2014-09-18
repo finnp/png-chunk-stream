a69669d052802053131f5dab6e28da1d09001dc9
Finn <derfinn@gmail.com>
2014-09-18 21:14:45 +0200

    inject

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ var equal = require('buffer-equal')
 
 var PNGsignature = new Buffer([137, 80, 78, 71, 13, 10, 26, 10])
 
-function PNG() {
+function PNG(opts) {
+  if(!(this instanceof PNG)) return new PNG(opts)
   this.nread = 0
   this.signature = null
   this.buffer = new Buffer(0)
-  Transform.apply(this)
+  this.injects = []
+  Transform.apply(this, opts)
 }
 inherits(PNG, Transform)
 
+PNG.prototype.inject = function inject(buffer) {
+  this.injects.push(buffer)
+}
+
 PNG.prototype._transform = function _transform(chunk, enc, cb) {   
-  this.push(chunk) 
   this.buffer = Buffer.concat([this.buffer, chunk]) // performance?
   this.nread += chunk.length
   
@@ -23,8 +28,8 @@ PNG.prototype._transform = function _transform(chunk, enc, cb) {
       this.nread -= 8
       this.signature = this.buffer.slice(0, 8)
       if(equal(this.signature, PNGsignature)) {
+        this.push(this.signature)
         this.buffer = this.buffer.slice(8)
-        // this.push({type: 'signature', {data: signature})
       } else {
         return cb(new Error('Not a PNG signature'))
       }
@@ -38,42 +43,41 @@ PNG.prototype._transform = function _transform(chunk, enc, cb) {
     
     if(this.nread >= 4 && !('length' in this.current)) {
       this.current.length =  this.buffer.readInt32BE(0)
+      this.push(this.buffer.slice(0,4))
       this.buffer = this.buffer.slice(4)
     }
     if(this.nread >= 8 && !('type' in this.current)) {
-      this.current.type = this.buffer.slice(0, 4).toString('ascii')
+      var type = this.buffer.slice(0, 4)
+      this.current.type = type.toString('ascii')
+      this.push(type)
       this.buffer = this.buffer.slice(4)
     }
     if(this.nread >= 8 + this.current.length && !('data' in this.current)) {
       this.current.data = this.buffer.slice(0, this.current.length)
+      this.push(this.current.data)
       this.buffer = this.buffer.slice(this.current.length)
     }
 
     if(this.nread >= 12 + this.current.length ) {
       this.current.crc = this.buffer.slice(0, 4)
+      this.push(this.current.crc)
       this.buffer = this.buffer.slice(4)
       this.nread = this.nread - 12 - this.current.length
       // this.push(this.current)
-      this.emit('chunk', this.current)
+      this.emit(this.current.type, this.current)
+      
+      // inject
+      if(this.current.type === 'IHDR') {
+        this.injects.forEach(function (buffer) {
+          this.push(buffer)
+        }.bind(this))
+      }
+      
       this.current = false
     } else {
       return cb()
     }
-  }  
-
+  }
 }
-
-// PNG.prototype.readChunk = function () {
-//   var chunk = {}
-//   chunk.length = this.buf.readInt32BE(0)
-//   this.buf = this.buf.slice(4)
-//   chunk.type = this.buf.slice(0, 4).toString('ascii')
-//   this.buf = this.buf.slice(4)
-//   chunk.data = this.buf.slice(0, chunk.length)
-//   this.buf = this.buf.slice(chunk.length)
-//   chunk.crc = this.buf.slice(0, 4)
-//   this.buf = this.buf.slice(4)
-//   return chunk
-// }  
 
 module.exports = PNG
